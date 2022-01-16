aa71f668458288b316750b673579369d7cd6465c
carlos_reyes <carlosarp2000@gmail.com>
2022-01-15 21:53:27 -0500

    testing

--- contracts/TasksContracts.sol
+++ contracts/TasksContracts.sol
@@ -3,7 +3,19 @@ pragma solidity  ^0.8.6;
 
 contract TasksContracts {
 
-    uint taskCounter = 0;
+    uint public taskCounter = 0;
+
+    constructor () {
+        createTask("my first task", "i need to do someting");
+    }
+
+    event TaskCreated(
+        uint id,
+        string title,
+        string description,
+        bool done,
+        uint createdAt
+    );
 
     struct Task {
         uint id;
@@ -16,10 +28,15 @@ contract TasksContracts {
     mapping (uint256 => Task ) public tasks;
 
     function createTask(string memory _title, string memory _description) public {
-        tasks[taskCounter] = Task(taskCounter, _title, _description, false, block.timestamp);
         taskCounter++;
+        tasks[taskCounter] = Task(taskCounter, _title, _description, false, block.timestamp);
+        emit TaskCreated(taskCounter, _title, _description, false, block.timestamp);
     }
 
-    //function toggleDone() {}
+    function toggleDone(uint _id) public {
+        Task memory _task = tasks[_id];
+        _task.done = !_task.done;
+        tasks[_id] = _task;
+    }
     
 }
---
+++ test/tasksContract.js
@@ -0,0 +1,42 @@
+const TasksContract = artifacts.require("TasksContracts");
+
+contract("TasksContract", () => {
+  before(async () => {
+    this.tasksContract = await TasksContract.deployed();
+  });
+
+  it("migrate deployed succesfully", async () => {
+    const address = this.tasksContract.address;
+
+    assert.notEqual(address, null);
+    assert.notEqual(address, undefined);
+    assert.notEqual(address, 0x0);
+    assert.notEqual(address, "");
+  });
+
+  it("get Tasks List", async () => {
+    const tasksCounter = await this.tasksContract.taskCounter();
+    const task = await this.tasksContract.tasks(tasksCounter);
+
+    assert.equal(task.id.toNumber(), tasksCounter);
+    assert.equal(task.title, "my first task");
+    assert.equal(task.description, "i need to do someting");
+    assert.equal(task.done, false);
+    assert.equal(tasksCounter, 1);
+  });
+
+  it("task created succesfully", async () => {
+    const result = await this.tasksContract.createTask(
+      "Some task",
+      "description two"
+    );
+    const taskevent = result.logs[0].args;
+    const tasksCounter = await this.tasksContract.taskCounter();
+
+    assert.equal(tasksCounter, 2);
+    assert.equal(taskevent.id.toNumber(), 2);
+    assert.equal(taskevent.title, "Some task");
+    assert.equal(taskevent.description, "description two");
+    assert.equal(taskevent.done, false);
+  });
+});
